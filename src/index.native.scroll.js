c7768d8f2a7bd75e2d5dcf7e193a2e61c0848eef
Tom Walters <tom.walters@cantarus.com>
2016-04-11 16:12:01 +0100

    Added offset calculation onHandleLayout

--- src/index.native.scroll.js
+++ src/index.native.scroll.js
@@ -187,6 +187,9 @@ class SwipeableViews extends React.Component {
     if (width) {
       this.setState({
         viewWidth: width,
+        offset: {
+          x: this.state.indexLatest * width,
+        },
       });
     }
   };
