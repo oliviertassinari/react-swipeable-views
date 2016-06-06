c5120c9e955b2ee2c9e5505faf247d616312608c
Olivier Tassinari <olivier.tassinari@gmail.com>
2016-06-06 23:14:19 +0200

    [eslint] Do some cleanup

---
+++ docs/src/.eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  rules: {
+    'react/jsx-no-literals': 'off',
+  }
+};
--- docs/src/app.js
+++ docs/src/app.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import Main from 'Main';
 
 require('normalize.less');
 require('stylesheet.less');
 require('github-light.less');
 
-ReactDOM.render(<Main />, document.getElementById('main'));
+render(<Main />, document.getElementById('main'));
