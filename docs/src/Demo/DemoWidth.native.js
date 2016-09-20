207cb03848a94e1efe5ec8511a80b35e6c83f331
Sergey Sadovoi <serg.sadovoi@gmail.com>
2016-09-20 17:00:37 +0300

    [Docs] Add native demo for Slide Width

---
+++ docs/src/Demo/DemoWidth.native.js
@@ -0,0 +1,51 @@
+// @flow weak
+
+import React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+} from 'react-native';
+import SwipeableViews from '../../../src/index.native.animated';
+
+const styles = StyleSheet.create({
+  slide: {
+    padding: 15,
+    height: 100,
+  },
+  slide1: {
+    backgroundColor: '#FEA900',
+  },
+  slide2: {
+    backgroundColor: '#B3DC4A',
+  },
+  slide3: {
+    backgroundColor: '#6AC0FF',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
+
+const DemoWidth = () => (
+  <SwipeableViews slideWidth={80} slideMargin={4}>
+    <View style={[styles.slide, styles.slide1]}>
+      <Text style={styles.text}>
+        slide n°1
+      </Text>
+    </View>
+    <View style={[styles.slide, styles.slide2]}>
+      <Text style={styles.text}>
+        slide n°2
+      </Text>
+    </View>
+    <View style={[styles.slide, styles.slide3]}>
+      <Text style={styles.text}>
+        slide n°3
+      </Text>
+    </View>
+  </SwipeableViews>
+);
+
+export default DemoWidth;
