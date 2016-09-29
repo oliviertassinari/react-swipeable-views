4428c761f83e99103785b6b5cdebe6129256a691
Jan Hartmann <jha@askcody.dk>
2016-09-29 09:08:57 +0200

    Missing return statement

--- src/autoPlay.js
+++ src/autoPlay.js
@@ -111,6 +111,7 @@ export default function autoPlay(MyComponent) {
 
       if (!autoplay) {
         clearInterval(this.timer);
+        return;
       }
 
       const indexLatest = this.state.index;
