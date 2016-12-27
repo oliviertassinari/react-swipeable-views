8c1f976bf7ebc8ff07c04a72cc122e1cd67dc968
Olivier Tassinari <olivier.tassinari@gmail.com>
2016-12-27 20:10:28 +0100

    [utils] Address the issue with the latest multi package migration
    Closes #217.

--- packages/react-swipeable-views/src/autoPlay.js
+++ packages/react-swipeable-views/src/autoPlay.js
@@ -2,5 +2,12 @@
 
 // Deprecated import
 import { autoPlay } from 'react-swipeable-views-utils';
+import warning from 'warning';
 
-export default autoPlay;
+export default function autoPlayDeprecated(MyComponent) {
+  warning(`react-swipeable-view: you are using the autoPlay Higer order Component from the
+react-swipeable-view package, please install react-swipeable-view-utils and use the module from this package.
+`);
+
+  return autoPlay(MyComponent);
+}
