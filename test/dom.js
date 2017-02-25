e2ec2e7860fbfafdabb18182efd88efa13f2c134
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-02-25 13:49:33 +0100

    test(js): simplify the mocha setup

---
+++ test/dom.js
@@ -0,0 +1,19 @@
+// @flow
+const { jsdom } = require('jsdom');
+
+function createDOM() {
+  global.document = jsdom('');
+  global.window = document.defaultView;
+
+  Object.keys(document.defaultView).forEach((property) => {
+    if (typeof global[property] === 'undefined') {
+      global[property] = document.defaultView[property];
+    }
+  });
+
+  global.navigator = {
+    userAgent: 'node.js',
+  };
+}
+
+module.exports = createDOM;
