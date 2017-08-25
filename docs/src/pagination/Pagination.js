bc9a01310db06d054361225a8c52c851ec9474b2
Olivier Tassinari <olivier.tassinari@gmail.com>
2017-08-25 20:59:24 +0200

    [core] Upgrade react-native (#330)

--- docs/src/pagination/Pagination.js
+++ docs/src/pagination/Pagination.js
@@ -1,7 +1,7 @@
 // @flow weak
 
-import React, { Component, PropTypes } from 'react';
-
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import PaginationDot from './PaginationDot';
 
 const styles = {
