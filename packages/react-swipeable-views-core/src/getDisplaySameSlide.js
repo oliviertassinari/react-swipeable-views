e9b4f1272619436f10a8918cf9846a9d72fc4ba4
stefano <stefano.davella@gmail.com>
2018-10-24 09:04:50 +0200

    fix getDisplaySameSlide

--- packages/react-swipeable-views-core/src/getDisplaySameSlide.js
+++ packages/react-swipeable-views-core/src/getDisplaySameSlide.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 const getDisplaySameSlide = (props, nextProps) => {
   let displaySameSlide = false;
-  const getChildrenKey = child => (child ? child.key : null);
+  const getChildrenKey = child => (child ? child.key : 'empty');
 
   if (props.children.length && nextProps.children.length) {
     const oldKeys = React.Children.map(props.children, getChildrenKey);
-    const oldKey = oldKeys[props.index] || null;
+    const oldKey = oldKeys[props.index];
 
     if (oldKey !== null && oldKey !== undefined) {
       const newKeys = React.Children.map(nextProps.children, getChildrenKey);
-      const newKey = newKeys[nextProps.index] || null;
+      const newKey = newKeys[nextProps.index];
 
       if (oldKey === newKey) {
         displaySameSlide = true;
