dc88897249ad6c4dc5ca06aeadf9686cc1a1b1a1
yacut <roman.rogozhnikov@gmail.com>
2018-10-28 20:26:31 +0100

    fix eslint

--- native/rn-cli.config.js
+++ native/rn-cli.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   getTransformModulePath() {
-    return require.resolve("react-native-typescript-transformer");
+    return require.resolve('react-native-typescript-transformer');
   },
   getSourceExts() {
-    return ["ts", "tsx"];
+    return ['ts', 'tsx'];
   }
 };
