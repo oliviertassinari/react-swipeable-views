01e5d6cd12ba333c4e49be1d24899dfb7a0361f7
benitogf <benitogf@outlook.com>
2019-08-21 18:32:18 +0800

    comment out setIndexCurrent

--- packages/react-swipeable-views/src/SwipeableViews.js
+++ packages/react-swipeable-views/src/SwipeableViews.js
@@ -404,9 +404,9 @@ class SwipeableViews extends React.Component {
 
       this.startIndex =
         -tranformNormalized.pageX /
-        (this.viewLength -
-          parseInt(rootStyle.paddingLeft, 10) -
-          parseInt(rootStyle.paddingRight, 10)) || 0;
+          (this.viewLength -
+            parseInt(rootStyle.paddingLeft, 10) -
+            parseInt(rootStyle.paddingRight, 10)) || 0;
     }
   };
 
@@ -737,18 +737,18 @@ class SwipeableViews extends React.Component {
     } = this.state;
     const touchEvents = !disabled
       ? {
-        onTouchStart: this.handleTouchStart,
-        onTouchEnd: this.handleTouchEnd,
-      }
+          onTouchStart: this.handleTouchStart,
+          onTouchEnd: this.handleTouchEnd,
+        }
       : {};
     const mouseEvents =
       !disabled && enableMouseEvents
         ? {
-          onMouseDown: this.handleMouseDown,
-          onMouseUp: this.handleMouseUp,
-          onMouseLeave: this.handleMouseLeave,
-          onMouseMove: this.handleMouseMove,
-        }
+            onMouseDown: this.handleMouseDown,
+            onMouseUp: this.handleMouseUp,
+            onMouseLeave: this.handleMouseLeave,
+            onMouseMove: this.handleMouseMove,
+          }
         : {};
 
     // There is no point to animate if we are already providing a height.
