c022f70c86cc5b2c3953802cf40833db04ceff78
kodai3 <k3dai.su3@gmail.com>
2020-08-04 11:48:00 +0900

    update

--- scripts/copy-files.js
+++ scripts/copy-files.js
@@ -8,24 +8,24 @@ const buildPath = path.join(packagePath, './lib');
 const srcPath = path.join(packagePath, './src');
 
 async function typescriptCopy({ from, to }) {
-    if (!(await fse.exists(to))) {
-        console.warn(`path ${to} does not exists`);
-        return [];
-    }
+  if (!(await fse.exists(to))) {
+    console.warn(`path ${to} does not exists`);
+    return [];
+  }
 
-    const files = glob.sync('**/*.d.ts', { cwd: from });
-    const cmds = files.map(file => fse.copy(path.resolve(from, file), path.resolve(to, file)));
-    return Promise.all(cmds);
+  const files = glob.sync('**/*.d.ts', { cwd: from });
+  const cmds = files.map(file => fse.copy(path.resolve(from, file), path.resolve(to, file)));
+  return Promise.all(cmds);
 }
 
 async function run() {
-    try {
-        // TypeScript
-        await typescriptCopy({ from: srcPath, to: buildPath });
-    } catch (err) {
-        console.error(err);
-        process.exit(1);
-    }
+  try {
+    // TypeScript
+    await typescriptCopy({ from: srcPath, to: buildPath });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 }
 
 run();
