5f8c92d232c3ff65a48c2e1c530d04a50b80d01b
A. Koepcke <akoepcke@users.noreply.github.com>
2020-10-02 21:24:09 +0200

    Update DemoResistance.js

--- docs/src/pages/demos/DemoResistance.js
+++ docs/src/pages/demos/DemoResistance.js
@@ -20,7 +20,7 @@ const styles = {
 
 function DemoResistance() {
   return (
-    <SwipeableViews resistance>
+    <SwipeableViews resistance enableMouseEvents>
       <div style={Object.assign({}, styles.slide, styles.slide1)}>slide n°1</div>
       <div style={Object.assign({}, styles.slide, styles.slide2)}>slide n°2</div>
       <div style={Object.assign({}, styles.slide, styles.slide3)}>slide n°3</div>
